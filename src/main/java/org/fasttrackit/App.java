9505138df18933149456b67a646335698f6730b4
Chiuaru Bogdan <bchiuaru94@gmail.com>
2019-06-18 20:52:17 +0300

    Tema cu exercitii basic

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,30 @@
+package org.fasttrackit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Exercise firstExercise = new Exercise();
+
+        firstExercise.sum(5,8);
+        firstExercise.divide(20,10);
+        firstExercise.multiply(120, 5);
+
+
+        Exercise secondExercise = new Exercise();
+
+        secondExercise.sum(20.5, 1.9);
+        secondExercise.divide(190.6,22.5);
+        secondExercise.multiply(11.5,20.5);
+
+
+    }
+
+
+
+    }
+
---
+++ src/main/java/org/fasttrackit/Exercise.java
@@ -0,0 +1,52 @@
+package org.fasttrackit;
+
+public class Exercise {
+//    metoda pentru adunare
+    double A;
+    double B;
+    double result;
+
+    public double sum(double A, double B){
+        double result = A + B;
+        System.out.println(" Suma este " + result);
+        return result;
+
+    }
+
+    public int sum(int A, int B) {
+        int result = A + B;
+        System.out.println(" Suma este " + result);
+        return result;
+    }
+//    metoda pentru impartire
+
+    public double divide(double A, double B){
+        double result = A / B;
+        System.out.println(" Rezultatul impartirii este" + result);
+        return result;
+
+    }
+
+    public int divide(int A, int B) {
+        int result = A / B;
+        System.out.println(" Rezultatul impartirii este " + result);
+        return result;
+    }
+//    metoda pentru inmultire
+
+    public double multiply(double A, double B) {
+        double result = A * B;
+        System.out.println(" Rezultatul inmultirii este " + result);
+        return result;
+    }
+
+public int multiply(int A, int B) {
+        int result = A * B;
+    System.out.println(" Rezultatul inmultirii este " + result);
+    return result;
+}
+
+
+
+}
+
