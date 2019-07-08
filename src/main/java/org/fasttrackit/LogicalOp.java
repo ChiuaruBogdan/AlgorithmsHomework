4d1c127a9547aa7f94f63636e18fc2510b7ea555
Chiuaru Bogdan <bchiuaru94@gmail.com>
2019-07-08 20:01:27 +0300

    Tema aproape completa

---
+++ src/main/java/org/fasttrackit/LogicalOp.java
@@ -0,0 +1,86 @@
+package org.fasttrackit;
+
+import javax.xml.transform.Result;
+
+public class LogicalOp {
+
+//first exercise
+//public void printNumbersLine (int startingNumber){
+//    for (int i = 1; i != 100; i++){
+//        System.out.print((startingNumber + i) + " ");
+//    }
+//}
+
+//second exercise
+
+//    public void printNumbersLine (int startingNumber){
+//        for (int i = 2; i >= -100; i--){
+//            System.out.println(i);
+//        }
+//    }
+
+
+// third exercise , trebuie introduse doua numeber in void main ca sa mearga
+//
+//    public void printNumbersLine( int startingNumber, int endingNumber){
+//        for (int i = startingNumber; i <= endingNumber; i++)
+//            System.out.println(i);
+//    }
+
+// fourth exercise
+//    public void printNumbersLine(int x, int y) {
+//        if(x<y) {
+//            for (int i = x; i <= y; i++){
+//                System.out.println(i);
+//            }
+//        }
+
+//    fifth exercise
+
+//    public void printNumbersLine(int startingNumber){
+//        for(int i = 0; i<=100; i++){
+//            if (i % 2 == 0){
+//                System.out.println(i);
+//
+//            }
+//        }
+
+//    sixth exercise
+//    public void printNumbersLine(int startingNumber){
+//        for(int i = 1; i<=100; i++) {
+//            if (i % 2 != 0) {
+//                System.out.println(i);
+//            }
+//        }
+
+//        seventh exercise
+//
+//   public void sum (int nr){
+//       int sum = 0;
+//       for (int i = nr; i <= 100; i++) {
+//           sum = sum + i;
+//       }
+//       System.out.println("The sum is = " + sum);
+//       }
+
+    public void sum (int nr){
+        int sum = 0;
+        int totalnr = 0;
+        for(int i = nr; i <= 100; i++) {
+            sum += i;
+            totalnr++;
+        }
+        double medie = (double)sum/totalnr;
+        System.out.println("Media este = " + medie);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
